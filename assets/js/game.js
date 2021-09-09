721637db0564f8b2092cec6c97b9b9e34b27670d
aziz <ahmed613@umn.edu>
2021-09-08 22:41:33 -0500

    while and for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,39 +7,46 @@ var playerAttack = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 
-var enemyName = "Roborto";
+var enemyNames= ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyAttack, enemyHealth);
 
-var fight = function() {
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames, enemyAttack, enemyHealth);
+
+console.log(enemyNames.length)
+
+var fight = function(enemyNames) {
+  while (enemyHealth > 0) {
      // Alert players that they are starting the round
  
-     window.alert("Welcome to Robot Gladiators!");
+     
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
  
    // if player choses to fight, then fight
-if (promptFight === "fight" || promptFight === "FIGHT") {
+   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
   
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
   
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
   
     // check player's health
@@ -65,7 +72,13 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
+ } 
 
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  //call fight-function with enemy robot
+  fight(pickedEnemyName);
+}
